fb2262b34ea4675fa267ba289a4e2e1c3949c034
HegePI <19744083+HegePI@users.noreply.github.com>
2019-03-24 09:43:56 +0200

    Laskarit viikko2

--- dokumentaatio/tyoaikakirjanpito.md
+++ dokumentaatio/tyoaikakirjanpito.md
@@ -17,14 +17,17 @@ __Mitä tehtiin__
 
 * Työaikakalenterin luominen
 
+* Viikko 2 laskareiden tekeminen, JUnit -testauksen harjoittelu
+
 __Aikaa kului__
 
-1h
+2,5h
 
 ## Koonnos
 
 |  PVM.   | aikaa käytetty | 
 | ------- | -------------- |
 | 22.3.19 | 2h             |
-|Yhteensä | 2h             |
+| 23.3.19 | 2,5h             |
+|Yhteensä | 4,5h             |
 
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,46 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void saldonLisaaminenOikein() {
+        kortti.lataaRahaa(5);
+
+        assertEquals("saldo: 15.0", kortti.toString());
+    }
+
+    @Test
+    public void saldonVahentaminenToimii() {
+        kortti.otaRahaa(5);
+
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+
+    @Test
+    public void eiMeneNegatiiviselle() {
+        kortti.otaRahaa(12);
+
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void trueKunRiittaa() {
+        boolean riittaa = kortti.otaRahaa(5);
+
+        assertEquals(riittaa, true);
+    }
+
+    @Test
+    public void falseKunEiRiita() {
+        boolean riittaa = kortti.otaRahaa(12);
+
+        assertEquals(riittaa, false);
     }
 }
