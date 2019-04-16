e6b8236d6ac370bd0ff6995647ee74fd11f6cfe2
Heikki <19744083+HegePI@users.noreply.github.com>
2019-04-16 13:19:58 +0300

    db muutoksia

--- sudokuapp/src/main/java/dao/Database.java
+++ sudokuapp/src/main/java/dao/Database.java
@@ -20,9 +20,11 @@ public class Database {
         String path = ("jdbc:sqlite:" + file.getAbsolutePath());
         return DriverManager.getConnection(path);
     }
+
     //Creates .db file and required tables, if directory doesn't contain it.
     private void initdb() {
         System.out.println("Alustetaan tietokanta");
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 }
